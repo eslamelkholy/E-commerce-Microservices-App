4e5108032f44f45fdc555a57e7815d6ace591ccf
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-04-21 17:27:54 +0200

    Solved Issue Within Docker Exit With Exit Code 0 By Adding no-daemon with PM2

--- docker/scripts/product.sh
+++ docker/scripts/product.sh
@@ -1,2 +1,2 @@
 echo "Starting Product Service"
-pm2 start ./config/pm2.products.config.js
+pm2 start ./config/pm2.products.config.js --name ProductService --log /var/log/pm2/pm2.log --watch --no-daemon
--- docker/scripts/user.sh
+++ docker/scripts/user.sh
@@ -1,2 +1,2 @@
 echo "Starting User Service"
-pm2 start ./config/pm2.user.config.js
+pm2 start ./config/pm2.user.config.js --name UserService --log /var/log/pm2/pm2.log --watch --no-daemon
