3cdc7c2f5920b2d8d9d3d77d718976a21592b5bd
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-05-12 22:41:24 +0200

    Testing CI/CD Pipleline With Jenkins

--- servers/products/loaders/express.ts
+++ servers/products/loaders/express.ts
@@ -9,7 +9,7 @@ import KafkaConsumer from '../../shared/services/kafka/KafkaConsumer';
 
 export default ({ app }: { app: Application }) => {
   app.get('/status', (req, res) => {
-    res.status(200).send('It Works Fine');
+    res.status(200).send('[Product-Server] Works Fine');
   });
 
   app.use(expressRequestId());
