ba66712767fb4348e88f0fd9605f0fda90639a26
eslamelkholy <eslamelkholy5325762@gmail.com>
2021-05-23 14:35:59 +0200

    Testing Github Actions (Web Hook)

--- servers/products/loaders/express.ts
+++ servers/products/loaders/express.ts
@@ -9,7 +9,7 @@ import KafkaConsumer from '../../shared/services/kafka/KafkaConsumer';
 
 export default ({ app }: { app: Application }) => {
   app.get('/status', (req, res) => {
-    res.status(200).send('[Product-Server] Works Fine With Pipeline');
+    res.status(200).send('[Product-Server] Testing Github Actions (Web Hook)');
   });
 
   app.use(expressRequestId());
